3887e3a6939f01d2aa7e32741df806ed30b8b37d
danhagg <danielhaggerty1976@gmail.com>
2017-11-09 11:29:05 -0600

    new

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var socket = require('socket.io');
 // App setup by invoking 'express' function
 var app = express();
 
-// Setup server as a variable to listen on a portal
-// Add function to let us know its listening
 var PORT = process.env.PORT || 4000;
 var server = app.listen(PORT, function () {
   console.log('listening to requests on ' + PORT);
@@ -34,9 +32,3 @@ io.on('connection', function (socket) {
     socket.broadcast.emit('typing', data);
   });
 });
-
-
-// var PORT = process.env.PORT || 4000;
-// app.listen(PORT, function () {
-// console.log('Listening on port ' + PORT);
-// });
